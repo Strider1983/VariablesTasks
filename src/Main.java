23b193bcbeb0b4dbbc8bc995e6e826d8522f4b5f
Strider <mezentsev.k@gmail.com>
2023-08-01 12:08:41 +0800

    Tasks 4-5 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Variables Tasks!");
+        System.out.println("Задача 6");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println("Вес боксера №1 " + boxer1 + " кг");
+        System.out.println("Вес боксера №2 " + boxer2 + " кг");
+        var totalweight = boxer1 + boxer2;
+        System.out.println("Общий вес боксеров " + totalweight + " кг");
+        System.out.println("Задача 7");
+        var weightdif = boxer2 - boxer1;
+        System.out.println("Разница в весе " + weightdif + " кг");
+        var weight_dif = boxer2 % boxer1;
+        System.out.println("Весовая разница " + weightdif + " кг");
     }
 }
