66cb3095b2172631dfdf08abaeb336d5bae5caa0
Strider <mezentsev.k@gmail.com>
2023-08-01 11:54:35 +0800

    Tasks 4-5 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Variables Tasks!");
+        var friend = 12;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
-}
+    }
